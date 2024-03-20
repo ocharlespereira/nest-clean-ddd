8ae914485e7987e514ee9bd5e9a4443c65346f31
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-20 17:13:48 -0300

    feat: create test e2e for create account

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,40 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  it('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'jon@example.com',
+      password: '123123',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDataBase = await prisma.user.findUnique({
+      where: {
+        email: 'jon@example.com',
+      },
+    })
+
+    expect(userOnDataBase).toBeTruthy()
+  })
 })
