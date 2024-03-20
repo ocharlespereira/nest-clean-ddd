305f6d6bd89fe7492217f3a982c6de6673076aeb
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-20 10:38:48 -0300

    feat: fetch recent list paginator questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionController } from './controllers/fetch-recent-question.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -19,6 +20,7 @@ import { PrismaService } from './prisma/prisma.service'
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionController,
   ],
   providers: [PrismaService],
 })
