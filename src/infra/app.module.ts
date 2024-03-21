85dd42a2bfa03f3606c73d2f2fc9b64cb5743dd4
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-21 11:14:24 -0300

    feat: move nest project for infra

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
-import { AuthenticateController } from './controllers/authenticate.controller'
-import { CreateAccountController } from './controllers/create-account.controller'
-import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchRecentQuestionController } from './controllers/fetch-recent-question.controller'
 import { envSchema } from './env'
+import { AuthenticateController } from './http/controllers/authenticate.controller'
+import { CreateAccountController } from './http/controllers/create-account.controller'
+import { CreateQuestionController } from './http/controllers/create-question.controller'
+import { FetchRecentQuestionController } from './http/controllers/fetch-recent-question.controller'
 import { PrismaService } from './prisma/prisma.service'
 
 @Module({
