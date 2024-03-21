b4018587461a4867e2104220feeae6cb871ac209
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-21 20:26:23 -0300

    chore: not important

--- src/core/either.spec.ts
+++ src/core/either.spec.ts
@@ -10,10 +10,6 @@ const doSomethig = (shoudSuccess: boolean): Either<string, string> => {
 test('success result', () => {
   const result = doSomethig(true)
 
-  if (result.isRight()) {
-    console.log('sult.isRight() :', result.value)
-  }
-
   expect(result.isRight()).toBe(true)
   expect(result.isLeft()).toBe(false)
 })
