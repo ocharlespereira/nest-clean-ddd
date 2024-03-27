3b2f0a463398ed8dceafb857ba3beae3adc7d21f
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-27 10:29:30 -0300

    teste2e:  upload create attachment controller

--- src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -36,5 +36,8 @@ describe('Upload Attachment (E2E)', () => {
       .attach('file', './test/e2e/myPhoto.jpeg')
 
     expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    })
   })
 })
