be9c3d54b99ca65cd2c81bac613f4374bc063ff1
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-27 17:23:31 -0300

    chore: edit question with attachment controller

--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -3,10 +3,12 @@ import {
   QuestionAttachement,
   QuestionAttachmentProps,
 } from '@/domain/forum/enterprise/entities/question-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export const makeQuestionAttachment = (
   override: Partial<QuestionAttachmentProps> = {},
-  id?: UniqueEntityID
+  id?: UniqueEntityID,
 ) => {
   const questionattachment = QuestionAttachement.create(
     {
@@ -14,8 +16,30 @@ export const makeQuestionAttachment = (
       attachmentId: new UniqueEntityID(),
       ...override,
     },
-    id
+    id,
   )
 
   return questionattachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {},
+  ): Promise<QuestionAttachement> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
