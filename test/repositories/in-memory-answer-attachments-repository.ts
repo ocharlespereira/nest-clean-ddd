21535adc522b829adee817df29d27f02327c0997
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-29 23:51:12 -0300

    Fix: fix create answer with attachments

--- test/repositories/in-memory-answer-attachments-repository.ts
+++ test/repositories/in-memory-answer-attachments-repository.ts
@@ -8,15 +8,27 @@ export class InMemoryAnswerAttachmentsRepository
 
   async findManyByAnswerId(answerId: string) {
     const answerAttachments = this.items.filter(
-      (item) => item.answerId.toString() === answerId
+      (item) => item.answerId.toString() === answerId,
     )
 
     return answerAttachments
   }
 
+  async createMany(attachments: AnswerAttachement[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: AnswerAttachement[]): Promise<void> {
+    const answerAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = answerAttachments
+  }
+
   async deleteManyByAnswerId(answerId: string) {
     const answerAttachments = this.items.filter(
-      (item) => item.answerId.toString() !== answerId
+      (item) => item.answerId.toString() !== answerId,
     )
 
     this.items = answerAttachments
