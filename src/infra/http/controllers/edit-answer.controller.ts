77ebfc1486a5a8a768f890222fcab271c88e5827
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-30 23:32:37 -0300

    chore: fix attachments for edit answers

--- src/infra/http/controllers/edit-answer.controller.ts
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -32,14 +32,14 @@ export class EditAnswerController {
     @CurrentUser() user: UserPayload,
     @Param('id') answerId: string,
   ) {
-    const { content } = body
+    const { content, attachments } = body
     const userId = user.sub
 
     const result = await this.editAnswer.execute({
       authorId: userId,
       answerId,
       content,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
     })
 
     if (result.isLeft()) {
