d263b3165f43244caecdc75c2ef34694c84838f9
CharlesPereira <charlesti.ptu@gmail.com>
2024-03-31 21:46:00 -0300

    chore: add authorName in the answerComments

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/values-objects/comment-with-author'
+import { InMemoryStudentRepository } from './in-memory-student-respository'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
+
+  constructor(private inMemoryStudentRepository: InMemoryStudentRepository) {}
 
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id)
@@ -25,13 +29,44 @@ export class InMemoryAnswerCommentsRepository
     return answerComment
   }
 
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComment = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.inMemoryStudentRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()}" does not exists.`,
+          )
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          authorId: comment.authorId,
+          author: author.name,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        })
+      })
+
+    return answerComment
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
 
   async delete(answerComment: AnswerComment) {
     const itemIndex = this.items.findIndex(
-      (item) => item.id === answerComment.id
+      (item) => item.id === answerComment.id,
     )
 
     this.items.splice(itemIndex, 1)
